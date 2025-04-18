e1ef1e714dca081ebce38f9821ebfac4141ee7cd
Andrew <andrejpopkov565@gmail.com>
2025-04-18 11:05:11 +0300

    added connection to mongoDB, start server, necessary middlewares

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import pino from 'pino-http';
+import cors from 'cors';
+import express from 'express';
+// import router from './routers/index.js';
+// поки що немає роутера
+import cookieParser from 'cookie-parser';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  //   app.use(router);
+
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
