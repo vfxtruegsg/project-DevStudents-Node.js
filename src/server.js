f61b481d240b938c0633a668674778760ad37d40
Andrew <andrejpopkov565@gmail.com>
2025-04-24 21:29:18 +0300

    added allowedOrigins

--- src/server.js
+++ src/server.js
@@ -22,13 +22,7 @@ export const startServer = () => {
 
   app.use(
     cors({
-      origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
+      origin: allowedOrigins,
       credentials: true,
     }),
   );
